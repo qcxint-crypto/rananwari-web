92fb7631dd967fc665d6af919d81a9589192d456
qcxint <81528379+qcxint-crypto@users.noreply.github.com>
2025-11-22 16:37:34 +0700

    Refactor loadGLTF to return original scene
    Refactor loadGLTF function to return the original scene instead of a clone.

--- lib/gltf-loader.ts
+++ lib/gltf-loader.ts
@@ -2,13 +2,13 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const loader = new GLTFLoader();
 
-export const loadGLTF = (url: string): Promise<{ scene: THREE.Group }> => {
-  return new Promise((resolve, reject) => {
+export function loadGLTF(url: string) {
+  return new Promise<{ scene: THREE.Group }>((resolve, reject) => {
     loader.load(
       url,
-      (gltf) => resolve({ scene: gltf.scene.clone() }),
+      (gltf) => resolve({ scene: gltf.scene }),
       undefined,
-      reject
+      (error) => reject(error)
     );
   });
-};
+}
