cb9cbfe037c3b96eeab068f17f733facd12df6a9
qcxint <81528379+qcxint-crypto@users.noreply.github.com>
2025-11-22 16:57:06 +0700

    Implement loading state with splash screen
    Added loading state with splash screen before rendering main content.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Profile from "./components/Profile";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
+import SplashScreen from "./components/SplashScreen"; //
+
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 6000); //Durasi splash
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <SplashScreen />;
+  }
+
   return (
-    <main className="min-h-screen flex items-center justify-center bg-black text-white">
-      <h1 className="text-3xl font-bold">Hello from Vercel (Test)</h1>
+    <main className="bg-black text-white">
+      <Navbar />
+      <Contact />
+      <Hero />
+      <Profile />
+      <Skills />
+      <Projects />
+      <Footer />
     </main>
   );
 }
+
