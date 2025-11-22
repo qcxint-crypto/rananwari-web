2f55176b358e0c9abf225bb58d0b44d52b66414b
qcxint <81528379+qcxint-crypto@users.noreply.github.com>
2025-11-22 16:42:37 +0700

    Update gltf-loader.ts

--- lib/gltf-loader.ts
+++ lib/gltf-loader.ts
@@ -3,12 +3,16 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 const loader = new GLTFLoader();
 
 export function loadGLTF(url: string) {
-  return new Promise<{ scene: THREE.Group }>((resolve, reject) => {
+  return new Promise<{ scene: any }>((resolve, reject) => {
     loader.load(
       url,
-      (gltf) => resolve({ scene: gltf.scene }),
+      (gltf: any) => {
+        resolve({ scene: gltf.scene });
+      },
       undefined,
-      (error) => reject(error)
+      (error: unknown) => {
+        reject(error);
+      }
     );
   });
 }
